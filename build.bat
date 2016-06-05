e48f8e6d7856a513fe19d6785cca5fb9c9bac805
Anton Sosnin <antsosnin@yandex.ru>
2016-06-05 10:06:22 +0600

    Change build script

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 RMDIR /s /q .\build
-xcopy /Y .\key .\build
-xcopy /Y .\*.html .\build
+xcopy /D /E /C /R /I /K /Y .\cfg .\build\cfg
+xcopy /D /E /C /R /I /K /Y .\src .\build\src
 nexe -i index.js -o ./build/server.exe
